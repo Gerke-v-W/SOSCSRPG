838b755a6b826692b821cfa698052b6d7f27105c
Gerke-v-W <S1161016@student.windesheim.nl>
2022-03-16 23:04:37 +0100

    added  GameItem and Weapon models and a ItemFactory

---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,31 @@
+﻿namespace Engine.Models;
+
+public class Weapon : GameItem
+{
+    public int MiniumumDamage { get; set; }
+    public int MaximumDamage { get; set; }
+    public Weapon(
+        int itemTypeId, 
+        string name, 
+        int price,
+        int miniumumDamage, 
+        int maximumDamage) : 
+        base(
+            itemTypeId, 
+            name, 
+            price)
+    {
+        MiniumumDamage = miniumumDamage;
+        MaximumDamage = maximumDamage;
+    }
+
+    public new Weapon Clone()
+    {
+        return new Weapon(
+            ItemTypeId,
+            Name,
+            Price,
+            MiniumumDamage,
+            MaximumDamage);
+    }
+}
